55d128ce87a2a4bc91a72c0f02ae4e815ab5667a
pikaybh <pikabh@naver.com>
2023-08-06 19:12:45 +0900

    202308061912

--- functions.py
+++ functions.py
@@ -16,37 +16,30 @@ class_names = {"기타" : ["기타"],
 
 
 class FineTuneModel:
-    def __init__(self, _API_key : str, model_name : str):
-        """_API_key : openai api key, model_name : model"""
+    def __init__(self, _API_key : str, model_name : str, class_list : list):
+        """_API_key : openai api key, model_name : model, class_list : list"""
         self.api_key = _API_key
         self.model = model_name
+        self.cls_list = class_list
+        self.num_cls = len(class_list)
 
         openai.api_key = self.api_key
 
 
-    def run(self, prompt : str, max_tokens=50, temperature=0.2, num_class=8) -> tuple:
-        res = openai.Completion.create(model=self.model, prompt=prompt, max_tokens=max_tokens, temperature=temperature, logprobs=num_class)
+    def run(self, prompt : str, max_tokens=50, temperature=0.2) -> tuple:
+        res = openai.Completion.create(model=self.model, prompt=prompt, max_tokens=max_tokens, temperature=temperature, logprobs=self.num_cls)
         ob = res['choices'][0]['text'].lstrip(' ').split(' ')[0]
-        if "기타" in ob:
-            ob = "기타"
-        elif "끼임" in ob or "깔림" in ob:
-            ob = "끼임, 깔림"
-        elif "넘어짐" in ob:
-            ob = "넘어짐"
-        elif "떨어짐" in ob:
-            ob = "떨어짐"
-        elif "물체" in ob or "맞음" in ob:
-            ob = "물체에 맞음"
-        elif "부딪" in ob:
-            ob = "부딪힘"
-        elif "불균형" in ob or "무리한" in ob:
-            ob = "불균형 및 무리한 동작"
-        elif "절단" in ob or "베임" in ob or "찔림" in ob:
-            ob = "절단, 베임, 찔림"
-        else:
-            print(f"[error] {ob}")
+        for idx, cls in enumerate(self.cls_list):
+            if cls in ob or cls.startswith(ob):
+                ob = cls
+                break
+            elif idx+1 >= len(self.cls_list):
+                print(f"[error] {ob}")
 
-        probs = top_logprobs = res['choices'][0]['logprobs']['top_logprobs'][0]
+        probs = .0
+        for v in dict(res['choices'][0]['logprobs']['top_logprobs'][0]).values():
+            tmp = exp(v)
+            probs = tmp if probs <= tmp else probs
 
         return ob, probs
 
